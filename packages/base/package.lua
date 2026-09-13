76bf9b4444dff22f877fabf9bea74d0f238dab6e
gretagen <besternet91@gmail.com>
2026-09-13 21:16:21 +0200

    base: provide thesh in the base system

--- packages/base/package.lua
+++ packages/base/package.lua
@@ -4,7 +4,7 @@ return {
   summary = "Haliade OS base system (metapackage)",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/base/base-1.0.tar.gz",
   sha256  = "b1cfe25b987423ed5a1b81ed9c2f43ac13d905d1f615c4b325df4bbcb3e67da0",
-  deps    = { "glibc", "gcc", "openrc", "haliade-utils", "bash", "coreutils", "tar", "squashfs-tools", "lua", "cfdisk", "bubblewrap", "util-linux", "zeta" },
+  deps    = { "glibc", "gcc", "openrc", "haliade-utils", "bash", "coreutils", "tar", "squashfs-tools", "lua", "cfdisk", "bubblewrap", "util-linux", "zeta", "thesh" },
   archive = { strip = 1 },
   test    = function(p)
     p:run("test -f " .. p.install_root .. "/usr/share/doc/base/README")
