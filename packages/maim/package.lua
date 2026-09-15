24e822ee9f54a1ee586f6fa5c495193fbadfc1e8
Gretagen ZETA <128843618+gretagen@users.noreply.github.com>
2026-09-15 10:36:56 +0200

    Add 'icu' as a dependency for maim package

--- packages/maim/package.lua
+++ packages/maim/package.lua
@@ -4,7 +4,7 @@ return {
   summary = "Screenshot utility that takes a shot of the screen X11 (maim = make image magick)",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-nemesis/refs/heads/main/maim/maim-5.8.2-1-x86_64.pkg.tar.zst",
   sha256  = "f16b25f1a823a2e1d3065ee233968dad8dbe2e505023075ee80621a19617ff90",
-  deps    = { "glibc", "slop", "glew", "libX11", "libXext", "libpng", "libXfixes", "libXcomposite", "libXrandr", "libjpeg-turbo", "libXrender", "libwebp" },
+  deps    = { "glibc", "slop", "glew", "libX11", "libXext", "libpng", "libXfixes", "libXcomposite", "libXrandr", "libjpeg-turbo", "libXrender", "libwebp", "icu" },
   archive = { strip = 1 },
   test    = function(p)
     p:run("test -x " .. p.install_root .. "/usr/bin/maim")
