2108263c6161acdf4aefde35f0228af92658dddf
gretagen <besternet91@gmail.com>
2026-09-15 20:47:54 +0200

    zeta: 1.0.22 — root check, provided/elevate messaging, random per-command greetings

--- packages/zeta/package.lua
+++ packages/zeta/package.lua
@@ -1,9 +1,9 @@
 return {
   name    = "zeta",
-  version = "1.0.21",
+  version = "1.0.22",
   summary = "Zeta package manager for Haliade OS",
-  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/zeta/zeta-1.0.21.tar.gz",
-  sha256  = "de1d564792afbdff24627f983ffcae79d648b063c290214df214a2a1fa346cf1",
+  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/zeta/zeta-1.0.22.tar.gz",
+  sha256  = "53a5a259332bd761d616fde4dfa77800b41aa8026c7b4c387b0067434c01450e",
   deps    = { "lua" },
   archive = { strip = 1 },
   test    = function(p)
