4e3c6d2a3162ba2eea39d3e1fb38425f27d8be00
gretagen <besternet91@gmail.com>
2026-09-15 21:18:01 +0200

    zeta: 1.0.23 — -Elevate renamed to -Transcend, root check, provided messaging, random greetings

--- packages/zeta/package.lua
+++ packages/zeta/package.lua
@@ -1,9 +1,9 @@
 return {
   name    = "zeta",
-  version = "1.0.22",
+  version = "1.0.23",
   summary = "Zeta package manager for Haliade OS",
-  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/zeta/zeta-1.0.22.tar.gz",
-  sha256  = "53a5a259332bd761d616fde4dfa77800b41aa8026c7b4c387b0067434c01450e",
+  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/zeta/zeta-1.0.23.tar.gz",
+  sha256  = "c6797d860efae886306331f8cdf9062c97e24660fac9945d8811ad1648d35054",
   deps    = { "lua" },
   archive = { strip = 1 },
   test    = function(p)
