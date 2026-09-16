656fd3caa2253292b9b6830ebfb42a7617a86345
gretagen <besternet91@gmail.com>
2026-09-16 19:53:24 +0200

    Bump genzee 1.0 -> 1.1

--- packages/genzee/package.lua
+++ packages/genzee/package.lua
@@ -1,9 +1,9 @@
 return {
   name    = "genzee",
-  version = "1.0",
+  version = "1.1",
   summary = "btrfs generation manager for Haliade OS (Limine boot entries)",
-  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/genzee/genzee-1.0.tar.gz",
-  sha256  = "402a64a5abe3c3bd83905e0d4fe37e06a25e3dfbcb59d29dc8c0028dec0811b6",
+  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/genzee/genzee-1.1.tar.gz",
+  sha256  = "81a18d603b7b41f0e501ed86fcf7aad7c161a7d9f456c19113176994ce90a2d6",
   deps    = { "bash", "btrfs-progs", "util-linux" },
   archive = { strip = 1 },
   test    = function(p)
