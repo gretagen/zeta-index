9ef3600d40892098e9d04ba787afb95f98b60fa7
gretagen <besternet91@gmail.com>
2026-09-16 13:59:50 +0200

    Fix declaration sync.lua stray char on line 2

--- packages/declaration/package.lua
+++ packages/declaration/package.lua
@@ -3,7 +3,7 @@ return {
   version = "1.0.0",
   summary = "Haliade OS declarative system configuration (haliade-synchronize)",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/declaration/declaration-1.0.0.tar.gz",
-  sha256  = "760ce4e055835bfbe8d1aae4147be5363227e2795661cfd70fdc48369318b97f",
+  sha256  = "a5eb7e226e0b5dc7927c1593d1569a90e6b8884d7396f018f1c6c9ba93100d29",
   deps    = { "lua" },
   archive = { strip = 1 },
   test    = function(p)
