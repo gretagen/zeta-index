73a7a028614a12f9ef62c0d1b09627fa64b87d67
gretagen <besternet91@gmail.com>
2026-09-17 06:50:48 +0200

    Bump declaration 1.0.0 -> 1.1.0 (iniswap generator, init swap support)

--- packages/declaration/package.lua
+++ packages/declaration/package.lua
@@ -1,9 +1,9 @@
 return {
   name    = "declaration",
-  version = "1.0.0",
+  version = "1.1.0",
   summary = "Haliade OS declarative system configuration (haliade-synchronize)",
-  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/declaration/declaration-1.0.0.tar.gz",
-  sha256  = "a5eb7e226e0b5dc7927c1593d1569a90e6b8884d7396f018f1c6c9ba93100d29",
+  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/declaration/declaration-1.1.0.tar.gz",
+  sha256  = "741e9c3435fcdd25925bc9868d5890a9b1451112600812d5260ef3bad1c76b2e",
   deps    = { "lua" },
   archive = { strip = 1 },
   test    = function(p)
