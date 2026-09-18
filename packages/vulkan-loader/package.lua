2f34218a033b1ae5f84dfb6e7ff9bc598c3522c8
gretagen <besternet91@gmail.com>
2026-09-18 09:26:15 +0200

    vulkan-loader 1.3.296 -> 1.4.357.0 (Arch binary, replaces source build)

--- packages/vulkan-loader/package.lua
+++ packages/vulkan-loader/package.lua
@@ -1,15 +1,11 @@
 return {
   name    = "vulkan-loader",
-  version = "1.3.296",
-  summary = "Vulkan ICD loader library",
-  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/vulkan-loader/vulkan-loader-1.3.296.tar.gz",
-  sha256  = "682d5323cf31308402c888599b375ebf15810f95d6d1a08ad2f525766becf99b",
-  deps    = { "glibc", "vulkan-headers", "libxcb", "libX11" },
-  build   = function(p)
-    p:run("cmake -B build -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_TESTS=OFF -DBUILD_WSI_XCB_SUPPORT=ON -DBUILD_WSI_XLIB_SUPPORT=ON -DBUILD_WSI_WAYLAND_SUPPORT=ON")
-    p:run("cmake --build build -j" .. "$(nproc)")
-    p:run("DESTDIR=" .. p.install_root .. " cmake --install build")
-  end,
+  version = "1.4.357.0",
+  summary = "Vulkan ICD loader library (Arch binary)",
+  url     = "https://raw.githubusercontent.com/gretagen/zeta-nemesis/main/vulkan-loader/vulkan-loader-1.4.357.0.pkg.tar.zst",
+  sha256  = nil,
+  deps    = { "glibc" },
+  archive = { extract_arch_pkg = true },
   test    = function(p)
     p:run("test -f " .. p.install_root .. "/usr/lib/libvulkan.so.1")
   end,
