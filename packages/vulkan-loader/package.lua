8b4ac879e55ef12ecee8660ac1a81399c5247973
gretagen <besternet91@gmail.com>
2026-09-18 10:03:15 +0200

    vulkan-loader: add sha256

--- packages/vulkan-loader/package.lua
+++ packages/vulkan-loader/package.lua
@@ -3,7 +3,7 @@ return {
   version = "1.4.357.0",
   summary = "Vulkan ICD loader library (Arch binary)",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-nemesis/main/vulkan-loader/vulkan-loader-1.4.357.0.pkg.tar.zst",
-  sha256  = nil,
+  sha256  = "9ed4c22afb7ec3204dc13e8714d8144d43926b8c6d0d8299e6b95215569cd499",
   deps    = { "glibc" },
   archive = { extract_arch_pkg = true },
   test    = function(p)
