02a5d7b06ff937bdc9d30c0bb678ec54914ff30e
gretagen <besternet91@gmail.com>
2026-09-18 13:16:06 +0200

    Bump zeta to 1.0.28 (quotes config toggle, symlink fix, typo fixes)

--- packages/zeta/package.lua
+++ packages/zeta/package.lua
@@ -1,9 +1,9 @@
 return {
   name    = "zeta",
-  version = "1.0.27",
+  version = "1.0.28",
   summary = "Zeta package manager for Haliade OS",
-  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/zeta/zeta-1.0.27.tar.gz",
-  sha256  = "4aad387867e2fc5cf2c9269b40da662f8f6171433db49beea41bea2bc28ffaa1",
+  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/zeta/zeta-1.0.28.tar.zst",
+  sha256  = "792cf53bdb8707b9183c4f7bdc0391f4d6e60c40bd903b633c9e73ee1f9a51a7",
   deps    = { "lua" },
   archive = { strip = 1 },
   test    = function(p)
