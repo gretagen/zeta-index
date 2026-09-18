562eac04b664a622a95c412b568ec130f917283c
gretagen <besternet91@gmail.com>
2026-09-18 14:04:49 +0200

    Bump zeta to 1.0.31 (remove commit-phase symlink escape check)

--- packages/zeta/package.lua
+++ packages/zeta/package.lua
@@ -1,9 +1,9 @@
 return {
   name    = "zeta",
-  version = "1.0.30",
+  version = "1.0.31",
   summary = "Zeta package manager for Haliade OS",
-  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/zeta/zeta-1.0.30.tar.zst",
-  sha256  = "9d25e190fc427e756597f4ad02680674ecafee286ed3db9f590b919ec1c28c8f",
+  url     = "https://raw.githubusercontent.com/gretagen/zeta-packages/refs/heads/main/packages/zeta/zeta-1.0.31.tar.zst",
+  sha256  = "c250b068681ae4fc2172eec23100969a777b89a2fe9b3adb1b82121e65b3c4ea",
   deps    = { "lua" },
   archive = { strip = 1 },
   test    = function(p)
