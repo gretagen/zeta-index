d9b05f6f620ac2e146b196d4a0fe954cbe480e8b
gretagen <besternet91@gmail.com>
2026-09-18 15:42:24 +0200

    Bump zeta to 1.0.32 (symlink fix, quotes config)

--- packages/zeta/package.lua
+++ packages/zeta/package.lua
@@ -1,9 +1,9 @@
 return {
   name    = "zeta",
-  version = "1.0.31",
+  version = "1.0.32",
   summary = "Zeta package manager for Haliade OS",
-  url     = "https://raw.githubusercontent.com/gretagen/zeta-constructs/refs/heads/main/packages/zeta/zeta-1.0.31.tar.zst",
-  sha256  = "c250b068681ae4fc2172eec23100969a777b89a2fe9b3adb1b82121e65b3c4ea",
+  url     = "https://raw.githubusercontent.com/gretagen/zeta-constructs/refs/heads/main/packages/zeta/zeta-1.0.32.tar.zst",
+  sha256  = "16544ff410094637d829529dc0be3cfcb08e4ffffde0a29cc09508b503aae5c6",
   deps    = { "lua" },
   archive = { strip = 1 },
   test    = function(p)
