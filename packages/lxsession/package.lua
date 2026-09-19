fc38861c84ff5158f3e132f42c360c1c268552ca
gretagen <besternet91@gmail.com>
2026-09-20 01:00:59 +0200

    Fix lxsession URL: include epoch (1:) in filename

--- packages/lxsession/package.lua
+++ packages/lxsession/package.lua
@@ -2,7 +2,7 @@ return {
   name    = "lxsession",
   version = "0.5.6",
   summary = "LXDE session manager (Arch binary)",
-  url     = "https://raw.githubusercontent.com/gretagen/zeta-nemesis/main/lxsession/lxsession-0.5.6-2-x86_64.pkg.tar.zst",
+  url     = "https://raw.githubusercontent.com/gretagen/zeta-nemesis/main/lxsession/lxsession-1%3A0.5.6-2-x86_64.pkg.tar.zst",
   sha256  = "e3b71ffbc3054a5b0af677e0695a90d794e3559a76301786476493e6250bd157",
   deps    = { "bash", "cairo", "gdk-pixbuf", "glib", "gtk3", "libX11", "polkit" },
   archive = { extract_arch_pkg = true },
