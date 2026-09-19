e2f360c7c6c07d5ed5ad4eb6a1f6933c8c6ff26e
gretagen <besternet91@gmail.com>
2026-09-19 21:44:27 +0200

    Fix feh: add libseccomp to deps

--- packages/feh/package.lua
+++ packages/feh/package.lua
@@ -4,7 +4,7 @@ return {
   summary = "Fast and light image viewer (Arch binary)",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-nemesis/main/feh/feh-3.13.1-1-x86_64.pkg.tar.zst",
   sha256  = "95ca385f9b59d7cbea1aa75c924ad405af22118013c8719cbea2637d910ebf9a",
-  deps    = { "curl", "file", "hicolor-icon-theme", "imlib2", "libexif", "libpng", "libX11", "libXinerama" },
+  deps    = { "curl", "file", "hicolor-icon-theme", "imlib2", "libexif", "libpng", "libseccomp", "libX11", "libXinerama" },
   archive = { extract_arch_pkg = true },
   test    = function(p)
     p:run("test -x " .. p.install_root .. "/usr/bin/feh")
