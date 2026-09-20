69e3c01742842c0e264367fbe5fe47368e8ad525
gretagen <besternet91@gmail.com>
2026-09-20 19:06:13 +0200

    zeta 1.0.39: fix archive layout (bash launcher + nested lib/)

--- packages/zeta/package.lua
+++ packages/zeta/package.lua
@@ -1,9 +1,9 @@
 return {
   name    = "zeta",
-  version = "1.0.38",
+  version = "1.0.39",
   summary = "Zeta package manager for Haliade OS",
-  url     = "https://raw.githubusercontent.com/gretagen/zeta-constructs/refs/heads/main/packages/zeta/zeta-1.0.38.tar.zst",
-  sha256  = "3d1e8e511d5c210631912dd1d4e4247a9bf88bf1f80f53b9a8bafe093fe5172c",
+  url     = "https://raw.githubusercontent.com/gretagen/zeta-constructs/refs/heads/main/packages/zeta/zeta-1.0.39.tar.zst",
+  sha256  = "e99da848f28b7161d7b5a881c933080a091a2a403bda9c5a7f2714531228426f",
   deps    = { "lua" },
   archive = { strip = 1 },
   test    = function(p)
