85e31c65f71adcc3eba48b23e943ba980d1c8e79
gretagen <besternet91@gmail.com>
2026-09-21 00:28:53 +0200

    libglvnd: remove circular mesa dependency

--- packages/libglvnd/package.lua
+++ packages/libglvnd/package.lua
@@ -4,7 +4,7 @@ return {
   summary = "GLVND EGL/GL libraries",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-nemesis/refs/heads/main/libglvnd/libglvnd-1.7.0-3-x86_64.pkg.tar.zst",
   sha256  = "633c2e95d7798d0ce7ee1b7fe970a22325f7f6d35b9f923bb61b223a80934437",
-  deps    = { "glibc", "libxext", "mesa", },
+  deps    = { "glibc", "libxext", },
   archive = { strip = 1 },
   test    = function(p)
     p:run("test -e " .. p.install_root .. "/usr/lib")
