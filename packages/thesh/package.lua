a442b985bf9e1f83ea4ea6c22ab5a2953305306a
gretagen <besternet91@gmail.com>
2026-09-21 21:43:35 +0200

    thesh 0.2.4

--- packages/thesh/package.lua
+++ packages/thesh/package.lua
@@ -1,9 +1,9 @@
 return {
   name    = "thesh",
-  version = "0.2.3",
+  version = "0.2.4",
   summary = "Custom standalone POSIX shell for Haliade OS (default shell, replaces bash)",
-  url     = "https://raw.githubusercontent.com/gretagen/zeta-constructs/refs/heads/main/packages/thesh/thesh-0.2.3.tar.gz",
-  sha256  = "ac70779b50a763c7d379ed9e4752fc821441abcff99e43235eb2f17a81f302d3",
+  url     = "https://raw.githubusercontent.com/gretagen/zeta-constructs/refs/heads/main/packages/thesh/thesh-0.2.4.tar.gz",
+  sha256  = "4e6776ef1a84ff2a7deb77ab4da16d847a9ad5619e69e3d26476a88ff25864ab",
   deps    = { "glibc" },
   archive = { strip = 1 },
   test    = function(p)
