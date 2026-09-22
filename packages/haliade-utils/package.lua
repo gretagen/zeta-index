ff42a31cda46f51dd84c3edda0c9685737a3d117
Gretagen ZETA <128843618+gretagen@users.noreply.github.com>
2026-09-22 09:45:13 +0200

    Update package dependencies in package.lua

--- packages/haliade-utils/package.lua
+++ packages/haliade-utils/package.lua
@@ -4,7 +4,7 @@ return {
   summary = "Haliade OS utility scripts (metapackage)",
   url     = "https://raw.githubusercontent.com/gretagen/zeta-constructs/refs/heads/main/packages/haliade-utils/haliade-utils-1.0.tar.gz",
   sha256  = "27e897236f6aad11b3b0de1e88ee64300c700ea9ccbcafeff37c198c3375b36a",
-  deps    = { "genzee", "genkernel", "subspace-scripts", "zstrappa", "iniswap", "declaration", "house-handlers" },
+  deps    = { "chronos", "genkernel", "subspace-scripts", "zstrappa", "iniswap", "declaration", "house-handlers" },
   archive = { strip = 1 },
   test    = function(p)
     p:run("test -f " .. p.install_root .. "/usr/share/doc/haliade-utils/README")
