c250f37d036d7bbcd46e34c35dd645ccef7db25b
gretagen <besternet91@gmail.com>
2026-09-25 11:24:39 +0200

    thesh 0.3.0: configurable prompt, colors, typing/guesser/corrector options

--- packages/thesh/package.lua
+++ packages/thesh/package.lua
@@ -1,9 +1,9 @@
 return {
   name    = "thesh",
-  version = "0.2.5",
+  version = "0.3.0",
   summary = "Custom standalone POSIX shell for Haliade OS (default shell, replaces bash)",
-  url     = "https://raw.githubusercontent.com/gretagen/zeta-constructs/refs/heads/main/packages/thesh/thesh-0.2.5.tar.gz",
-  sha256  = "19777b9bb57287fa0a01f06a644111157b6c4acb79f67566943618eb387dc6d2",
+  url     = "https://raw.githubusercontent.com/gretagen/zeta-constructs/refs/heads/main/packages/thesh/thesh-0.3.0.tar.gz",
+  sha256  = "91ab896f2ec3e48dc1e10414749fb413e9bc204ea8b33f4d3c242a1856b922ca",
   deps    = { "glibc" },
   archive = { strip = 1 },
   test    = function(p)
